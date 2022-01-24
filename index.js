595b0deefca040891b2b9593c1f8952523a16c84
Sprakerdash <97488820+Sprakerdash@users.noreply.github.com>
2022-01-24 16:26:45 +1100

    .
    .

--- index.js
+++ index.js
@@ -25,78 +25,17 @@ const ccxt = require ('./ccxt/ccxt.js')
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* -------------------------------------------Handlebars-------------------------------------------------- */
-
-
+/* -------------------------------------------Handlebars ------------------------------------------ */
 
 
 // use body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-// API KEY pk_138be20ab85d47619223bd237b4fd511
-// create call_api function
-function call_api(finishedAPI, ticker) {
-	request('wss://stream.binance.com:9443' + ticker + '************', { json: true }, (err, res, body) => {
-	if (err) {return console.log(err);}
-	if (res.statusCode === 200){
-		//console.log(body);
-		finishedAPI(body);
-		};
-	});
-};
-
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const otherstuff = "mmmmmmmmmmm";
-
-// Set handlebar index GET route
-app.get('/', function (req, res) {
-	call_api(function(doneAPI) {
-			res.render('home', {
-	    	stock: doneAPI,
-    	});
-	}, "BTCUSDT");
-		
-});
-
-
-// Set handlebar index POST route
-app.post('/', function (req, res) {
-	call_api(function(doneAPI) {
-			//console.log(doneAPI);
-			//posted_stuff = req.body.stock_ticker;
-			res.render('home', {
-	    	stock: doneAPI,
-    	});
-	}, req.body.stock_ticker);
-		
-});
 
 // create about page route
 app.get('/about.html', function (req, res) {
@@ -108,4 +47,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
+
+
+
+
 
